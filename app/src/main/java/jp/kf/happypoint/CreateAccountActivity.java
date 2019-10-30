80cfedf3d5eb3a7ae846da213c7fa82876aa3bbb
Konoka Fujiwara <ririmaru2003@gmail.com>
2019-10-30 23:48:42 +0900

    finish login and account things

--- app/src/main/java/jp/kf/happypoint/CreateAccountActivity.java
+++ app/src/main/java/jp/kf/happypoint/CreateAccountActivity.java
@@ -1,14 +1,190 @@
 package jp.kf.happypoint;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import jp.kf.happypoint.R;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    EditText mEmailEditText;
+    EditText mPasswordEditText;
+    EditText mNameEditText;
+    ProgressDialog mProgress;
+    Intent mIntent;
+
+    FirebaseAuth mAuth;
+    OnCompleteListener<AuthResult> mCreateAccountListener;
+    OnCompleteListener<AuthResult> mLoginListener;
+    DatabaseReference mDataBaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        mDataBaseReference = FirebaseDatabase.getInstance().getReference();
+
+        // FirebaseAuthのオブジェクトを取得する
+        mAuth = FirebaseAuth.getInstance();
+
+        // アカウント作成処理のリスナー
+        mCreateAccountListener = new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // 成功した場合
+                    String email = mEmailEditText.getText().toString();
+                    String password = mPasswordEditText.getText().toString();
+
+                    // ログインを行う
+                    login(email, password);
+
+                    mIntent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(mIntent);
+
+                } else {
+                    // 失敗した場合
+                    // エラーを表示する
+                    View view = findViewById(android.R.id.content);
+                    Snackbar.make(view, "アカウント作成に失敗しました", Snackbar.LENGTH_LONG).show();
+
+                    // プログレスダイアログを非表示にする
+                    mProgress.dismiss();
+                }
+            }
+        };
+
+        // ログイン処理のリスナー
+        mLoginListener = new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+                    // 成功した場合
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    DatabaseReference userRef = mDataBaseReference.child(Const.UsersPATH).child(user.getUid());
+
+                    // アカウント作成の時は表示名をFirebaseに保存する
+                    String name = mNameEditText.getText().toString();
+
+                    Map<String, String> data = new HashMap<String, String>();
+                    data.put("name", name);
+                    userRef.setValue(data);
+
+                    // 表示名をPrefarenceに保存する
+                    saveName(name);
+
+                    userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot snapshot) {
+                            Map data = (Map) snapshot.getValue();
+                        }
+                        @Override
+                        public void onCancelled(DatabaseError firebaseError) {
+                        }
+                    });
+
+                    // プログレスダイアログを非表示にする
+                    mProgress.dismiss();
+
+                    // Activityを閉じる
+                    finish();
+
+                } else {
+                    // 失敗した場合
+                    // エラーを表示する
+                    View view = findViewById(android.R.id.content);
+                    Snackbar.make(view, "ログインに失敗しました", Snackbar.LENGTH_LONG).show();
+
+                    // プログレスダイアログを非表示にする
+                    mProgress.dismiss();
+                }
+            }
+        };
+
+
+        // UIの準備
+        setTitle("アカウント作成");
+
+        mEmailEditText = (EditText) findViewById(R.id.emailText);
+        mPasswordEditText = (EditText) findViewById(R.id.passwordText);
+        mNameEditText = (EditText) findViewById(R.id.nameText);
+
+        mProgress = new ProgressDialog(this);
+        mProgress.setMessage("処理中...");
+
+        Button createButton = (Button) findViewById(R.id.createButton);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // キーボードが出てたら閉じる
+                InputMethodManager im = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                im.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+                String email = mEmailEditText.getText().toString();
+                String password = mPasswordEditText.getText().toString();
+                String name = mNameEditText.getText().toString();
+
+                if (email.length() != 0 && password.length() >= 6 && name.length() != 0) {
+                    // ログイン時に表示名を保存するようにフラグを立てる
+
+                    createAccount(email, password);
+                } else {
+                    // エラーを表示する
+                    Snackbar.make(v, "正しく入力してください", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
+    private void createAccount(String email, String password) {
+        // プログレスダイアログを表示する
+        mProgress.show();
+
+        // アカウントを作成する
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(mCreateAccountListener);
+    }
+
+    private void login(String email, String password) {
+        // プログレスダイアログを表示する
+        mProgress.show();
+
+        // ログインする
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(mLoginListener);
+    }
+
+    private void saveName(String name) {
+        // Preferenceに保存する
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(Const.NameKEY, name);
+        editor.commit();
     }
 }
